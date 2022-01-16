b882d1e6f866df9cb85b5d7620243ecabe9a0543
Artur U <artur.uff@gmail.com>
2022-01-16 22:44:38 +0200

    url change

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:webfeed/webfeed.dart';
-import 'package:http/io_client.dart';
+//import 'package:http/io_client.dart';
+import 'package:http/http.dart' as http;
 
 void main() {
   runApp(const MyApp());
@@ -28,20 +29,58 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final _urlController = TextEditingController();
+  String _url = 'https://rss.dw.com/xml/rss-ru-all';
+//
+//https://www.liga.net/news/world/rss.xml
   Future parseRSS() async {
-    final client = IOClient(HttpClient()
-      ..badCertificateCallback =
-          ((X509Certificate cert, String host, int port) => true));
-
-    // RSS feed
-    var response =
-        await client.get(Uri.parse('https://www.liga.net/news/world/rss.xml'));
+    // final client = IOClient(HttpClient()
+    //   ..badCertificateCallback =
+    //       ((X509Certificate cert, String host, int port) => true));
+    // // RSS feed
+    var response = await http.get(Uri.parse(_url));
     var channel = RssFeed.parse(response.body);
     var item = channel.items;
-    print(channel);
-
-    client.close();
+    // client.close();
     return item;
+  }
+
+  void changeURL() {
+    setState(() {
+      _url = _urlController.text;
+    });
+    parseRSS();
+  }
+
+  Future<void> _showMyDialog() async {
+    return showDialog<void>(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: <Widget>[
+                const Text('Enter a valid URL'),
+                TextField(
+                  controller: _urlController,
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                changeURL();
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 
   @override
@@ -53,43 +92,53 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('RSS Feed'),
-      ),
-      body: StreamBuilder(
-        stream: parseRSS().asStream(),
-        builder: <AsyncSnapshot>(context, snapshot) {
-          if (snapshot.hasData) {
-            return ListView.builder(
-              itemCount: snapshot.data.length,
-              itemBuilder: (context, int index) {
-                return Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Card(
-                      child: ListTile(
-                    title: Text(snapshot.data[index].title),
-                    subtitle: Text(snapshot.data[index].description),
-                    leading: Text((1 + index).toString()),
-                  )),
-                );
-              },
+        appBar: AppBar(
+          title: const Text('RSS Feed'),
+          actions: [
+            const Spacer(),
+            IconButton(
+                onPressed: _showMyDialog, icon: Icon(Icons.change_circle))
+          ],
+        ),
+        body: StreamBuilder(
+          stream: parseRSS().asStream(),
+          builder: <AsyncSnapshot>(context, snapshot) {
+            if (snapshot.hasData) {
+              return ListView.builder(
+                itemCount: snapshot.data.length,
+                itemBuilder: (context, int index) {
+                  return Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Card(
+                        child: ListTile(
+                      title: Text(snapshot.data[index].title),
+                      subtitle: Text(snapshot.data[index].description),
+                      leading: Text((1 + index).toString()),
+                    )),
+                  );
+                },
+              );
+            } else if (snapshot.hasError ||
+                snapshot.connectionState == ConnectionState.none) {
+              return Center(
+                child: Text(snapshot.error.toString()),
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
             );
-          } else if (snapshot.hasError ||
-              snapshot.connectionState == ConnectionState.none) {
-            return Center(
-              child: Text(snapshot.error.toString()),
-            );
-          }
-          return Center(
-            child: CircularProgressIndicator(),
-          );
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: parseRSS,
-        tooltip: 'Increment',
-        child: const Icon(Icons.refresh),
-      ),
-    );
+          },
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            setState(() {
+              parseRSS();
+            });
+          },
+          tooltip: 'Change',
+          child: const Icon(
+            Icons.change_circle,
+          ),
+        ));
   }
 }
